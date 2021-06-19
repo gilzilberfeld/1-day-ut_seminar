56c7ffa1f63eb880220e0644887f7977c3ca456d
gilzilberfeld <gil.zilberfeld@gmail.com>
2021-06-19 12:58:48 +0300

    coverage example

---
+++ src/test/java/testingil/unittesting/examples/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package testingil.unittesting.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
